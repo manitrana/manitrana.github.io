d596043dcade8a2741ca051a1fe77a29fe906a1f
manitrana <manitrana41@gmail.com>
2022-10-10 00:21:32 -0400

    added 2nd high-level widget

--- src/App.js
+++ src/App.js
@@ -30,21 +30,6 @@ function App() {
             <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
-               
-      
-
-        {/* High-Level Widgets */}
-        <Container sx = {{ p: '2' }}>
-          <Grid container spacing={12} justifyContent="center">
-            <Grid item xs={2}>
-              <Container sx={{ bgcolor: '#F8F8F8', height: '10vh', width: '200px', boxShadow: '1', borderRadius: '5px' }}>
-                <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
-                  Total Crimes          
-                </Typography>
-              </Container>          
-            </Grid>
-          </Grid>
-        </Container>
 
 
       {/* Total Crimes High-Level Widget */}
@@ -60,7 +45,27 @@ function App() {
             +10%
           </Box>
 
-          <Box sx={{ color: '#46505A', display: 'inline', fontSize: 14 }}>
+          <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>
+            vs. last month
+          </Box>
+
+        </Container>
+
+
+        {/* Violent Crimes High-Level Widget */}
+        <Container sx={{bgcolor: 'spacegray', boxShadow: 1, borderRadius: 2, p: 2, width: '300px'}}>
+          
+          <Box sx={{ color: '#46505A' }}>Total Violent Crimes</Box>
+
+          <Box sx={{ color: '#173A5E', fontSize: 24, fontWeight: 'medium' }}>
+            47.1 K
+          </Box>
+
+          <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>
+            +7%
+          </Box>
+
+          <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>
             vs. last month
           </Box>
 
