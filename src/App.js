7383b0967fe726d309370a2d2753d6c95da6c9ba
manitrana <manitrana41@gmail.com>
2022-10-10 00:01:12 -0400

    Integrating changes form the main branch for Appjs

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/material/MenuItem';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      {
+      /* Nav Bar element */
+      }
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Crime Analysis Dashboard
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+               
+      {/* Test button */}
+      <Button>Test Button</Button>
+
+      {/* High-Level Widgets */}
+      <Container sx = {{ p: '2' }}>
+        <Grid container spacing={12} justifyContent="center">
+          <Grid item xs={2}>
+            <Container sx={{ bgcolor: '#F8F8F8', height: '10vh', width: '200px', boxShadow: '1', borderRadius: '5px' }}>
+              <Typography variant="body1" component="div" sx={{ flexGrow: 1 }}>
+                Total Crimes          
+              </Typography>
+            </Container>          
+          </Grid>
+        </Grid>
+      </Container>
+
+    </div>
+  );
+}
+
+export default App;
