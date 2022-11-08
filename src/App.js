5685b9b6618dc3f421f033ce0e22fa788e702433
manitrana <manitrana41@gmail.com>
2022-11-08 14:04:16 -0500

    mui icons added in high-level widgets

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ import * as d3 from 'd3';
 import { Line } from 'react-chartjs-2';
 import records from "./socrata_metadata.json";
 import { useEffect, useRef, useState } from "react";
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import HandshakeIcon from '@mui/icons-material/Handshake';
 
 
 
@@ -57,31 +60,56 @@ function App() {
           <Container sx=
           {{
             display: 'flex', 
-            justifyContent: 'center', 
+            justifyContent: 'flex-start', 
             alignItems: 'center', 
             bgcolor: 'white', 
             border: '1px solid ghostwhite', 
-            /*boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', */
             borderRadius: 2, 
             p: 2, 
             width: '400px', 
             height: '150px'}}>
-            <Box sx={{alignItems: 'center'}}>
+
+            <Box sx={{alignItems: 'start'}}>
+              
+              <Box>
+                <HandshakeIcon fontSize='large'/>
+              </Box>
+
               <Box sx={{ color: '#46505A' }}>Total Sales</Box>
+
               <Box sx={{color: '#173A5E', fontSize: 24, fontWeight: 'medium' }}>128.3 K</Box>
-              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>+10%</Box>
-              <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>vs. last month</Box>
+              
+              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>
+                <ArrowUpwardIcon/>
+                10%
+                <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>vs. last month</Box>
+              </Box>
+              
             </Box>
           </Container>
         </Grid>
 
         {/* Violent Crimes High-Level Widget */}
         <Grid item xs={3}>
-          <Container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', bgcolor: 'white', border: '1px solid ghostwhite', borderRadius: 2, p: 2, width: '400px', height: '150px'}}>       
+          <Container sx=
+          {{
+            display: 'flex', 
+            justifyContent: 'flex-start', 
+            alignItems: 'center', 
+            bgcolor: 'white', 
+            border: '1px solid ghostwhite', 
+            borderRadius: 2, 
+            p: 2, 
+            width: '400px', 
+            height: '150px'}}>   
+
             <Box sx={{alignItems: 'center'}}>
               <Box sx={{ color: '#46505A' }}>Total Revenue</Box>
               <Box sx={{ color: '#173A5E', fontSize: 24, fontWeight: 'medium' }}>47.1 K</Box>
-              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>+7%</Box>
+              <Box sx={{ color: 'red', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>
+                <ArrowDownwardIcon/>
+                7%
+              </Box>
               <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>vs. last month</Box>
             </Box>
           </Container>
@@ -89,11 +117,23 @@ function App() {
 
         {/* Assaults High-Level Widget */}
         <Grid item xs={3}>
-          <Container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', bgcolor: 'white', border: '1px solid ghostwhite', borderRadius: 2, p: 2, width: '400px', height: '150px'}}>       
-            <Box sx={{alignItems: 'center'}}>
+          <Container sx=
+          {{
+            display: 'flex', 
+            justifyContent: 'flex-start', 
+            bgcolor: 'white', 
+            border: '1px solid ghostwhite', 
+            borderRadius: 2, 
+            p: 2, 
+            width: '400px', 
+            height: '150px'}}>       
+            <Box>
               <Box sx={{ color: '#46505A' }}>Total Products Sold</Box>
               <Box sx={{ color: '#173A5E', fontSize: 24, fontWeight: 'medium' }}>20 K</Box>
-              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>+17%</Box>
+              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>
+                <ArrowUpwardIcon/>
+                17%
+              </Box>
               <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>vs. last month</Box>
             </Box>
           </Container>
@@ -101,11 +141,24 @@ function App() {
 
         {/* Robberies High-Level Widget */}
         <Grid item xs={3}>
-          <Container sx={{display: 'flex', justifyContent: 'center',alignItems: 'center', bgcolor: 'white', border: '1px solid ghostwhite', borderRadius: 2, p: 2, width: '400px', height: '150px'}}>       
+          <Container sx=
+          {{
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            bgcolor: 'white', 
+            border: '1px solid ghostwhite', 
+            borderRadius: 2, 
+            p: 2, 
+            width: '400px', 
+            height: '150px'}}>       
             <Box sx={{alignItems: 'center'}}>
               <Box sx={{ color: '#46505A' }}>Total Distributors</Box>
               <Box sx={{ color: '#173A5E', fontSize: 24, fontWeight: 'medium' }}>20 K</Box>
-              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>+17%</Box>
+              <Box sx={{ color: '#009688', display: 'inline', fontWeight: 'bold', mx: 0.5, fontSize: 14,}}>
+                <ArrowUpwardIcon/>
+                17%
+              </Box>
               <Box sx={{ color: 'lightgray', display: 'inline', fontSize: 14 }}>vs. last month</Box>
             </Box>
           </Container>
